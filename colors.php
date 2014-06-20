2d80570c4b6c615fccfcb304e95523b5ba2ad2d8
Kiindersurprise <stevenweb@hotmail.fr>
2014-06-20 20:17:55 +0200

    Patch final avant implémentation nuked.

---
+++ colors.php
@@ -0,0 +1,13 @@
+<?php
+/************************************************
+*	Thème Impact_Nk pour Nuked Klan
+*	Design :  (http://www.best-creative.fr/)
+*	Codage : fce (http://www.best-creative.fr/)
+************************************************/
+    defined('INDEX_CHECK') or die;
+
+    $bgcolor1 = $couleur[1];
+    $bgcolor2 = $couleur[2];
+    $bgcolor3 = $couleur[3];
+    $bgcolor4 = $couleur[4];
+?>
